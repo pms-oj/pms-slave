da4abea2486e61a8fcdbfd080c3d37e880fdf190
Yeri Kim <laplacianmatrix@gmail.com>
2022-09-28 01:17:02 +0000

    `src/main.rs`: split to `src/protocol.rs`

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,17 @@ extern crate lazy_static;
 mod config;
 mod container;
 mod language;
+mod protocol;
 
-use async_std::net::TcpStream;
-use async_std::prelude::*;
-use async_std::task::{spawn};
+pub const CONFIG_FILE: &'static str = "config.toml";
 
-use bincode::Options;
-
-use judge_protocol::packet::*;
-use k256::ecdh::EphemeralSecret;
-use k256::ecdh::SharedSecret;
-use k256::PublicKey;
-use rand::thread_rng;
 use std::fs::read_to_string;
 
-use async_std::sync::*;
+use log::{info, debug};
 
 use config::Config;
 use language::Languages;
-use std::pin::Pin;
-
-use log::{debug, info};
-
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
-pub const CONFIG_FILE: &'static str = "config.toml";
+use protocol::open_protocol;
 
 lazy_static! {
     static ref CONFIG: Config = {
@@ -38,77 +25,7 @@ lazy_static! {
     static ref LANGUAGES: Languages = Languages::load().expect("Some error occured");
 }
 
-struct State {
-    key: Arc<EphemeralSecret>,
-    shared: Arc<Mutex<Option<SharedSecret>>>,
-}
-
-impl State {
-    async fn handle_command(&mut self, _stream: &mut TcpStream, packet: Packet) {
-        match packet.heady.header.command {
-            Command::Handshake => {
-                if let Ok(server_pubkey) = bincode::DefaultOptions::new()
-                    .with_big_endian()
-                    .with_fixint_encoding()
-                    .deserialize::<PublicKey>(&packet.heady.body)
-                {
-                    self.shared =
-                        Arc::new(Mutex::new(Some(self.key.diffie_hellman(&server_pubkey))));
-                } else {
-                    debug!("[PMS-slave] An error occurred");
-                }
-            },
-            Command::ReqVerifyToken => {
-                
-            },
-            _ => {
-                debug!("[PMS-slave] An unknown command has received");
-                // Unknown
-            }
-        }
-    }
-}
-
 #[async_std::main]
 async fn main() {
-    loop {
-        // do master connection loop
-        if let Ok(_stream) = TcpStream::connect(CONFIG.master).await {
-            let stream: Arc<Mutex<TcpStream>> = Arc::new(Mutex::new(_stream));
-            let key = EphemeralSecret::random(thread_rng());
-            let state = Arc::new(Mutex::new(State {
-                key: Arc::new(key),
-                shared: Arc::new(Mutex::new(None)),
-            }));
-            // Send Handshake packet
-            let Handshake = Packet::make_packet(
-                Command::Handshake,
-                bincode::DefaultOptions::new()
-                    .with_big_endian()
-                    .with_fixint_encoding()
-                    .serialize(&state.lock().await.key.public_key())
-                    .unwrap(),
-            );
-            Handshake.send(Pin::new(stream.lock().await.by_ref())).await.ok();
-            loop {
-                // TODO: check connection
-                // packet loop
-                if let Ok(packet) =
-                    Packet::from_stream(Pin::new(stream.lock().await.by_ref())).await
-                {
-                    let state_mutex = Arc::clone(&state);
-                    let stream_mutex = Arc::clone(&stream);
-                    spawn(async move {
-                        state_mutex
-                            .lock()
-                            .await
-                            .handle_command(stream_mutex.lock().await.by_ref(), packet)
-                            .await
-                    });
-                } else {
-                    debug!("Wrong packet as received");
-                }
-            }
-        }
-    }
+    open_protocol().await
 }
---
+++ src/protocol.rs
@@ -0,0 +1,136 @@
+use async_std::net::TcpStream;
+use async_std::prelude::*;
+use async_std::task::{spawn};
+
+use bincode::Options;
+
+use judge_protocol::packet::*;
+use judge_protocol::handshake::*;
+use k256::ecdh::EphemeralSecret;
+use k256::ecdh::SharedSecret;
+use k256::PublicKey;
+use rand::thread_rng;
+use std::fs::read_to_string;
+
+use async_std::channel::{Sender, Receiver, unbounded};
+use async_std::sync::*;
+
+use std::pin::Pin;
+
+use log::{debug, info};
+use crate::{CONFIG, LANGUAGES};
+
+#[derive(Clone, Copy, Debug)]
+enum Actions {
+    Reconnect,
+    Unknown,
+}
+
+struct State {
+    key: Arc<EphemeralSecret>,
+    node_id: Mutex<u32>,
+    shared: Arc<Mutex<Option<SharedSecret>>>,
+    signal: Arc<Mutex<Sender<Actions>>>,
+}
+
+impl State {
+    async fn verify_token(&mut self, mut stream: &mut TcpStream) -> async_std::io::Result<()> {
+        let body = BodyAfterHandshake::<PublicKey> {
+            node_id: (*self.node_id.lock().await),
+            req: self.key.public_key(),
+        };
+        let packet = Packet::make_packet(Command::VerifyToken, body.bytes());
+        packet.send(Pin::new(&mut stream)).await
+    }
+    
+    async fn handle_command(&mut self, stream: &mut TcpStream, packet: Packet) {
+        match packet.heady.header.command {
+            Command::Handshake => {
+                if let Ok(res) = bincode::DefaultOptions::new()
+                    .with_big_endian()
+                    .with_fixint_encoding()
+                    .deserialize::<HandshakeResult>(&packet.heady.body)
+                {
+                    self.node_id = Mutex::new(res.node_id);
+                    self.shared =
+                        Arc::new(Mutex::new(Some(self.key.diffie_hellman(&res.server_pubkey))));
+                } else {
+                    debug!("[PMS-slave] An error occurred");
+                }
+            },
+            Command::ReqVerifyToken => {
+                if let Ok(state) = bincode::DefaultOptions::new()
+                .with_big_endian()
+                .with_fixint_encoding()
+                .deserialize::<bool>(&packet.heady.body) {
+                    if !state {
+                        debug!("[PMS-slave] Session was expired. Trying to reconnect ...");
+                        self.signal.lock().await.send(Actions::Reconnect).await;
+                    }
+                } else {
+                    debug!("[PMS-slave] An error occurred");
+                }
+            },
+            _ => {
+                debug!("[PMS-slave] An unknown command has received");
+                // Unknown
+            }
+        }
+    }
+}
+
+pub async fn open_protocol() {
+    loop {
+        // do master connection loop
+        if let Ok(_stream) = TcpStream::connect(CONFIG.master).await {
+            let stream: Arc<Mutex<TcpStream>> = Arc::new(Mutex::new(_stream));
+            let key = EphemeralSecret::random(thread_rng());
+            let (send, recv): (Sender<Actions>, Receiver<Actions>) = unbounded();
+            let state = Arc::new(Mutex::new(State {
+                key: Arc::new(key),
+                node_id: Mutex::new(std::u32::MAX),
+                shared: Arc::new(Mutex::new(None)),
+                signal: Arc::new(Mutex::new(send)),
+            }));
+            // Send Handshake packet
+            let Handshake = Packet::make_packet(
+                Command::Handshake,
+                bincode::DefaultOptions::new()
+                    .with_big_endian()
+                    .with_fixint_encoding()
+                    .serialize(&state.lock().await.key.public_key())
+                    .unwrap(),
+            );
+            Handshake.send(Pin::new(stream.lock().await.by_ref())).await.ok();
+            loop {
+                if let Ok(actions) = recv.try_recv() {
+                    match actions {
+                        Actions::Reconnect => {
+                            break;
+                        },
+                        _ => {}
+                    }
+                }
+                // TODO: check connection
+                // packet loop
+                if let Ok(packet) =
+                    Packet::from_stream(Pin::new(stream.lock().await.by_ref())).await
+                {
+                    let state_mutex = Arc::clone(&state);
+                    let stream_mutex = Arc::clone(&stream);
+                    spawn(async move {
+                        state_mutex
+                            .lock()
+                            .await
+                            .handle_command(stream_mutex.lock().await.by_ref(), packet)
+                            .await
+                    });
+                } else {
+                    debug!("Wrong packet as received");
+                }
+            }
+            drop(state);
+            drop(recv);
+        }
+    }
+}
